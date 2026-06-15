4215170bf69a804a982d07c0a62456629929348e
Wesley Medeiros <Wesley.coelho@outlook.com>
2026-06-15 20:38:28 -0300

    :wrench: chore: Initial setup

---
+++ src/daily_diet_api_python/app.py
@@ -0,0 +1,24 @@
+import os
+from time import time
+
+from dotenv import load_dotenv
+from flask import Flask
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+
+@app.route("/health", methods=["GET"])
+def health_check() -> dict[str, int | str]:
+  return {"project": "python-api", "time": int(time())}
+
+
+def run() -> None:
+  app.run(debug=True, port=3333)  # noqa: S201
+
+
+if __name__ == "__main__":
+  run()
