28de7976ff1f5976affbfa8067a4eb068fe33309
Wesley Medeiros <Wesley.coelho@outlook.com>
2026-06-15 20:40:26 -0300

    :wrench: chore: Database setup

--- src/daily_diet_api_python/app.py
+++ src/daily_diet_api_python/app.py
@@ -4,11 +4,15 @@ from time import time
 from dotenv import load_dotenv
 from flask import Flask
 
+from src.daily_diet_api_python.database import db
+
 load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+db.init_app(app)
 
 
 @app.route("/health", methods=["GET"])
